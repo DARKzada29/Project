272fc3d421d4361e0ed8637c3523b9af2708aae2
DARKzada29 <166631119+DARKzada29@users.noreply.github.com>
2025-05-22 11:01:20 -0300

    Update App.jsx

--- Project/frontend/src/App.jsx
+++ Project/frontend/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
             <Route path="/carrinho" element={<CarrinhoPage />} />
             <Route path="/servicos" element={<ServicesPage />} />
             <Route path="/contato" element={<div className="container" style={{minHeight: "80vh", padding: "4rem 1rem"}}>Página de Contato</div>} />
-            <Route path="/perfil" element={<ProfilePage /> } />
+            <Route path="/perfil" element={<ProfilePage />} />
           </Routes>
         </Router>
       </AnimationProvider>
